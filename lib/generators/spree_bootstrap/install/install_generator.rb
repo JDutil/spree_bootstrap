0feefbb4296ddae65257313786609bcdfb663232
Jeff Dutil <jdutil@burlingtonwebapps.com>
2013-09-17 05:55:13 -0400

    Remove skeleton by overriding spree_frontend.css.

--- lib/generators/spree_bootstrap/install/install_generator.rb
+++ lib/generators/spree_bootstrap/install/install_generator.rb
@@ -6,12 +6,12 @@ module SpreeBootstrap
 
       def add_javascripts
         append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_bootstrap\n"
-        append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_bootstrap\n"
+        # append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_bootstrap\n"
       end
 
       def add_stylesheets
         inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_bootstrap\n", :before => /\*\//, :verbose => true
-        inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_bootstrap\n", :before => /\*\//, :verbose => true
+        # inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_bootstrap\n", :before => /\*\//, :verbose => true
       end
 
       def add_migrations
